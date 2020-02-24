80d2be8786caf4d1485386e687dfdcb0c900b4db
Lubo Sladok <lubomir.sladok@bsp.sk>
2020-02-24 11:56:40 +0100

    android studio initial application

---
+++ app/src/main/java/online/fixu/bsp/alf/alfnotifworker/MainActivity.java
@@ -0,0 +1,13 @@
+package online.fixu.bsp.alf.alfnotifworker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
