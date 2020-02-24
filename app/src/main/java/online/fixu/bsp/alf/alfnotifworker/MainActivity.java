7e3fec2f90980dea914fee50ed92efc7046bd1aa
Lubo Sladok <lubomir.sladok@bsp.sk>
2020-02-24 13:19:49 +0100

    add workflows button

--- app/src/main/java/online/fixu/bsp/alf/alfnotifworker/MainActivity.java
+++ app/src/main/java/online/fixu/bsp/alf/alfnotifworker/MainActivity.java
@@ -2,12 +2,23 @@ package online.fixu.bsp.alf.alfnotifworker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+
+        Log.d(TAG, "onClick()");
+
+
     }
 }
