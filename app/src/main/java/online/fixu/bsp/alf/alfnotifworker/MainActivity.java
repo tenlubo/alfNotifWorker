dc9985f85cdc171a631371951e158a75080792c5
Lubo Sladok <lubomir.sladok@bsp.sk>
2020-02-24 14:35:51 +0100

    add workflow activity

--- app/src/main/java/online/fixu/bsp/alf/alfnotifworker/MainActivity.java
+++ app/src/main/java/online/fixu/bsp/alf/alfnotifworker/MainActivity.java
@@ -1,6 +1,8 @@
 package online.fixu.bsp.alf.alfnotifworker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,7 +20,11 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
 
         Log.d(TAG, "onClick()");
-
-
+        try {
+            Intent k = new Intent(this, WorkFlowActivity.class);
+            startActivity(k);
+        } catch(Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
     }
 }
---
+++ app/src/main/java/online/fixu/bsp/alf/alfnotifworker/WorkFlowActivity.java
@@ -0,0 +1,16 @@
+package online.fixu.bsp.alf.alfnotifworker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkFlowActivity extends AppCompatActivity {
+
+    public static final String TAG = "WorkFlowActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_flow);
+    }
+}
