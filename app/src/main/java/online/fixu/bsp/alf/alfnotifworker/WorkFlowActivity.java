aa778c26ca428cd3bf3a162336db401a03eec211
Lubo Sladok <lubomir.sladok@bsp.sk>
2020-02-24 15:22:55 +0100

    add workflow model

--- app/src/main/java/online/fixu/bsp/alf/alfnotifworker/WorkFlowActivity.java
+++ app/src/main/java/online/fixu/bsp/alf/alfnotifworker/WorkFlowActivity.java
@@ -1,10 +1,15 @@
 package online.fixu.bsp.alf.alfnotifworker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import online.fixu.bsp.alf.alfnotifworker.worker.WorkflowViewModel;
+
 public class WorkFlowActivity extends AppCompatActivity {
+
+    private WorkflowViewModel mViewModel;
 
     public static final String TAG = "WorkFlowActivity";
 
@@ -12,5 +17,8 @@ public class WorkFlowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_flow);
+
+        // Get the ViewModel
+        mViewModel = new ViewModelProvider(this).get(WorkflowViewModel.class);
     }
 }
---
+++ app/src/main/java/online/fixu/bsp/alf/alfnotifworker/worker/WorkflowViewModel.java
@@ -0,0 +1,13 @@
+package online.fixu.bsp.alf.alfnotifworker.worker;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class WorkflowViewModel extends AndroidViewModel {
+
+    public WorkflowViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
